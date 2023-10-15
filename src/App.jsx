6eb4ced950d63ccca034d48e1ca14c0504b9e140
mkrtum <mkrtum.abrahamyan66@gmail.com>
2023-10-15 12:41:12 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
 
-import {Routes, Route, Navigate} from 'react-router-dom';
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import HomePage from "./pages/homePage";
 // import MoodBoardPage from "./pages/moodBoardPage";
@@ -13,21 +13,20 @@ import Footer from "./components/footer";
 import PageNotFound from "./pages/pageNotFound";
 
 function App() {
-    return (
-        <div className="App">
-            <Header/>
-            <Routes>
-                <Route path={'/'} element={<HomePage/>}/>
-                {/*<Route path={'/moodboard'} element={<MoodBoardPage/>}/>*/}
-                <Route path={'/projects'} element={<ProjectsPage/>}/>
-                <Route path={'/services'} element={<ServicesPage/>}/>
-                <Route path={'/contactUs'} element={<ContactUsPage/>}/>
-                <Route path={'/404'} element={<PageNotFound/>}/>
-                <Route path={'/*'} element={<Navigate to='/'/>}/>
-            </Routes>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+        <Route path={"/projects"} element={<ProjectsPage />} />
+        <Route path={"/services"} element={<ServicesPage />} />
+        <Route path={"/contactUs"} element={<ContactUsPage />} />
+        <Route path={"/404"} element={<PageNotFound />} />
+        <Route path={"/*"} element={<Navigate to="/" />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
