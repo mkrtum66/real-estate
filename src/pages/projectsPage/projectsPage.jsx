8e7fcff6ef7b66535783103a1ab39d97361d34cd
Mkrtum Abrahamyan <mkrtumabrahamyan@Mkrtums-MacBook-Pro.local>
2023-10-15 20:11:43 -0700

    chnages

--- src/pages/projectsPage/projectsPage.jsx
+++ src/pages/projectsPage/projectsPage.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import './projectsPage.scss'
+import React from "react";
+import "./projectsPage.scss";
 
-import {Container} from "react-bootstrap";
-import CompareProjects from "../../components/CompareImage";
+import { Container } from "react-bootstrap";
 
 const ProjectsPage = () => {
-    return (
-        <div className='projects-page'>
-            <Container>
-                <p className='title'>Before & After</p>
-                <CompareProjects/>
-                <div className="mb-5"></div>
-            </Container>
-        </div>
-    );
+  return (
+    <div className="projects-page">
+      <Container>
+        <p className="title">Before & After</p>
+        <div className="mb-5"></div>
+      </Container>
+    </div>
+  );
 };
 
 export default ProjectsPage;
